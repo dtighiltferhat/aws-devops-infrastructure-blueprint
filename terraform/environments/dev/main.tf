9c7c7786d82cd27bb7771c95480d895f7d762c3d
dtighiltferhat <dtighiltferhat@gmail.com>
2026-02-18 15:43:37 -0500

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -12,3 +12,22 @@ module "vpc" {
 
   tags = var.tags
 }
+
+module "alb" {
+  source = "../../modules/alb"
+
+  name        = var.name
+  environment = var.environment
+
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+
+  # App defaults (we'll align EC2 to these later)
+  listener_port      = 80
+  listener_protocol  = "HTTP"
+  target_port        = 80
+  target_protocol    = "HTTP"
+  health_check_path  = "/"
+
+  tags = var.tags
+}
