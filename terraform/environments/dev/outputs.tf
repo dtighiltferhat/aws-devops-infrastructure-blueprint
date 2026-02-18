8b8053639b3924bcd0122124db6c00d36402acc7
dtighiltferhat <dtighiltferhat@gmail.com>
2026-02-18 15:44:15 -0500

    Update outputs.tf

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -13,3 +13,16 @@ output "private_subnet_ids" {
 output "nat_gateway_ids" {
   value = module.vpc.nat_gateway_ids
 }
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "alb_sg_id" {
+  value = module.alb.alb_sg_id
+}
+
+output "target_group_arn" {
+  value = module.alb.target_group_arn
+}
+
