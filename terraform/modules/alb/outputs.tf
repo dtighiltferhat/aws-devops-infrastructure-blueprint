66c95281eda530c07d1a36110926815131422f38
dtighiltferhat <dtighiltferhat@gmail.com>
2026-02-18 15:42:28 -0500

    Create outputs.tf

---
+++ terraform/modules/alb/outputs.tf
@@ -0,0 +1,24 @@
+output "alb_arn" {
+  description = "ALB ARN"
+  value       = aws_lb.this.arn
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = aws_lb.this.dns_name
+}
+
+output "alb_sg_id" {
+  description = "ALB security group ID"
+  value       = aws_security_group.alb.id
+}
+
+output "target_group_arn" {
+  description = "Target group ARN"
+  value       = aws_lb_target_group.this.arn
+}
+
+output "listener_arn" {
+  description = "Listener ARN"
+  value       = aws_lb_listener.http.arn
+}
