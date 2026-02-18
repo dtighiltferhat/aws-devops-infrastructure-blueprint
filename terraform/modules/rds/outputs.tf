8bddf469e2f5a28941ba2e38d51794d3c3b6e319
dtighiltferhat <dtighiltferhat@gmail.com>
2026-02-18 16:10:03 -0500

    Create outputs.tf

---
+++ terraform/modules/rds/outputs.tf
@@ -0,0 +1,24 @@
+output "db_endpoint" {
+  description = "Database endpoint"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "db_address" {
+  description = "Database hostname/address"
+  value       = aws_db_instance.this.address
+}
+
+output "db_port" {
+  description = "Database port"
+  value       = aws_db_instance.this.port
+}
+
+output "db_sg_id" {
+  description = "Database security group ID"
+  value       = aws_security_group.db.id
+}
+
+output "db_subnet_group" {
+  description = "DB subnet group name"
+  value       = aws_db_subnet_group.this.name
+}
