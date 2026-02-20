aa342372dc7992aefca6981b083436497c7be9de
dtighiltferhat <dtighiltferhat@gmail.com>
2026-02-20 14:52:13 -0500

    Update variables.tf

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -1,20 +1,42 @@
-variable "name"        { type = string }
-variable "environment" { type = string }
-variable "tags"        { type = map(string) default = {} }
+variable "name"        { 
+  type = string 
+}
+variable "environment" {
+  type = string 
+}
+variable "tags"        { 
+  type = map(string) 
+  default = {} 
+}
 
 # ALB / Target Group
-variable "alb_arn_suffix"         { type = string }
-variable "target_group_arn_suffix" { type = string }
+variable "alb_arn_suffix"         {
+  type = string 
+}
+
+variable "target_group_arn_suffix" {
+  type = string 
+}
 
 # ASG
-variable "asg_name" { type = string }
+variable "asg_name" { 
+  type = string 
+}
 
 # RDS
-variable "db_identifier" { type = string }
+variable "db_identifier" {
+  type = string 
+}
 
 # Optional notifications
 variable "sns_topic_arn" {
   description = "Optional SNS topic ARN for alarm notifications"
   type        = string
   default     = ""
+}
+
+variable "rds_free_storage_threshold_bytes" {
+  description = "Alarm threshold for RDS FreeStorageSpace in bytes"
+  type        = number
+  default     = 2147483648
 }
